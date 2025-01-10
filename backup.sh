fefb922868671c92a77a4d205354a87d73b926ca
ronaldo.brisa <familiaberohi@gmail.com>
2025-01-09 23:43:23 -0300

    Backup

--- backup.sh
+++ backup.sh
@@ -7,7 +7,7 @@ backup_dir="/home/ronaldobrisa/Docker"
 backup_file="backup_Docker-$(date +%d-%m-%Y-%H:%M:%S).tar.gz"
 
 # Create the backup
-tar -czf $backup_dir/$backup_file ~/mnt/backups-docker
+tar -czf $backup_dir/$backup_file ./mnt/backups-docker
 
 # Remove backups older than 30 days
 find $backup_dir -name "backup-*.tar.gz" -type f -mtime +30 -exec rm {} \;
