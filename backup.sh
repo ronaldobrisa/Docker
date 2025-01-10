cb0d7dc946eb6256e5fbb40bb1a4333554cb1e13
ronaldo.brisa <familiaberohi@gmail.com>
2025-01-09 23:45:02 -0300

    Backup

--- backup.sh
+++ backup.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash 
 
-# Define the backup directory
+# Define the backup directory origin
 backup_dir="/home/ronaldobrisa/Docker"
 
 # Define the backup file name
 backup_file="backup_Docker-$(date +%d-%m-%Y-%H:%M:%S).tar.gz"
 
-# Create the backup
-tar -czf $backup_dir/$backup_file ./mnt/backups-docker
+# Create the backup destiny directory
+tar -czf $backup_dir/$backup_file /mnt/backups-docker
 
 # Remove backups older than 30 days
 find $backup_dir -name "backup-*.tar.gz" -type f -mtime +30 -exec rm {} \;
